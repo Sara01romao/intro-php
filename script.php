55cdeff70d13bb5133e0b595f119e755df51e9b6
Sara Romão <sara-romao@live.com>
2021-03-31 17:18:07 -0300

    validação de campos do formulario

--- script.php
+++ script.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
 $categoria =[];
-
 $categoria[] = 'infantil';
 $categoria[] = 'adolecente';
 $categoria[] = 'adulto';
@@ -17,6 +14,33 @@ $age = $_POST['age'];
 
 //echo $name;
 //echo $age;
+
+
+//Validação de dados. 
+
+if(empty($name)){
+  echo 'Informe o nome';
+  return;
+}//empty: verifica se o campo não está vazio
+
+
+if(strlen($name)<3){
+  echo 'O nome deve ter mais de 3 caracteres';
+  return;
+}//stelen: conta a quantidade de caracter que a string tem 
+
+if(strlen($name)>10){
+  echo 'Nome é muito extenso';
+  return;
+}//controla a quantidade de caracteres da string
+
+
+if(!is_numeric($age)){
+
+  echo 'Infome o numero da idade';
+  return;
+
+}//verifica se não é um numero
 
 
 if($age >= 6 && $age <=12){
